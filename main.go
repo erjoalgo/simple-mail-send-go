155fb0052be0438effd6ad6124d0539a0f5f966b
Ernesto Alfonso <erjoalgo@gmail.com>
2015-11-28 14:14:49 -0500

    main flag options

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+)
 
+func main() {
+	var certPem string
+	var keyPem string
+	var address string
+	flag.StringVar(&certPem, "certPem", "", "cert.pem file location")
+	flag.StringVar(&keyPem, "keyPem", "", "key.pem file location")
+	flag.StringVar(&address, "address", "", "listen address")
+	flag.Parse()
+	if address == "" {
+		log.Fatal("address argument required")
+	} else if keyPem == "" || certPem == "" {
+		log.Fatal("cert, key files required")
+	} else {
+		log.Fatal("%v", ServeTLS(address, certPem, keyPem))
+	}
 }
 
 // Local Variables:
-// compile-cmd: "go run server.go services.go service.go mailRequest.go main.go "
+// compile-cmd: "go test"
 // End:
